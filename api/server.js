7184c62a4bee1f35b6da9899ec2c34a5bb2fc62c
moshudougee <mbuthiadenis@gmail.com>
2025-03-02 03:44:21 +0300

    update vercel.json and server.js

--- api/server.js
+++ api/server.js
@@ -1,11 +1,20 @@
-const jsonServer = require("json-server");
+import jsonServer from "json-server";
+import { createServer } from "http";
+
 const server = jsonServer.create();
-const router = jsonServer.router("api/db.json"); // Ensure db.json exists
+const router = jsonServer.router("api/db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-module.exports = (req, res) => {
+export default function handler(req, res) {
   server(req, res);
-};
+}
+
+if (process.env.NODE_ENV !== "production") {
+  createServer(server).listen(5000, () => {
+    console.log("JSON Server is running locally on port 5000");
+  });
+}
+
